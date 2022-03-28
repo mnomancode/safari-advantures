88adfc374e0b1a1a7d1ac9d7d3d87042eb181c90
Noman9k <chnoman503@gmail.com>
2022-03-28 17:13:13 +0000

    login By Sheraz

--- index.js
+++ index.js
@@ -1,46 +1,32 @@
 const firebaseConfig = {
     apiKey: "AIzaSyD5RNZY92nEvpK1CKI0u8dOdgSJ-2dhcog",
+authDomain: "safari-adventure-78671.firebaseapp.com",
+  projectId: "safari-adventure-78671",
+  storageBucket: "safari-adventure-78671.appspot.com",
+  messagingSenderId: "139068250812",
+  appId: "1:139068250812:web:ec4e8e5e4bb62a31b5ceb9",
+  measurementId: "G-YV5GKS22MS"
 
-    authDomain: "safari-adventure-78671.firebaseapp.com",
+  };
+  
 
-    projectId: "safari-adventure-78671",
-
-    storageBucket: "safari-adventure-78671.appspot.com",
-
-    messagingSenderId: "139068250812",
-
-    appId: "1:139068250812:web:1e7631c85fe63b80b5ceb9",
-
-    measurementId: "G-1HC9F98R52"
-
-};
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-
-var agentForm = document.getElementById("agentRegisterationForm").elements;
-
-// add All The Properties from the agent tabel in erd diagram
-var agentData = {
-    name: "Agent Name",
-    country: "USA",
-    verified: "false",
-
-};
-
-
 function agentCreation() {
+    var agentForm = document.getElementById("agentRegisterationForm").elements;
     const agentEmail = agentForm.item(0).value;
     const agentPassword = agentForm.item(1).value;
 
+
     firebase.auth().createUserWithEmailAndPassword(agentEmail, agentPassword)
         .then((userCredential) => {
-            console.log(agentEmail);
-
             // Signed in 
             var user = userCredential.user;
-            console.log(user);
             saveAgentToDb();
+
             alert('agent Creation Success full');
+
         })
         .catch((error) => {
             alert(error);
@@ -48,7 +34,30 @@ function agentCreation() {
 
 }
 
+function agentLogin() {
+    var agentForm = document.getElementById("agentSigninForm").elements;
+    const agentEmail = agentForm.item(0).value;
+    const agentPassword = agentForm.item(1).value;
 
+
+    firebase.auth().signInWithEmailAndPassword(agentEmail, agentPassword)
+        .then((userCredential) => {
+            // Signed in 
+            var user = userCredential.user;
+            saveAgentToDb();
+
+            alert('agent login Successfully');
+
+        })
+        .catch((error) => {
+            alert(error);
+        });
+
+}
+
+// function logout(){
+//     firebase.auth().signOut() 
+// }
 
 function saveAgentToDb() {
     var db = firebase.firestore();
@@ -56,7 +65,11 @@ function saveAgentToDb() {
     var userId = firebase.auth().currentUser.uid;
 
 
-    db.collection("agents").doc(userId).set(agentData)
+    db.collection("agents").doc(userId).set({
+        name: "Agent Name",
+        verified: "false",
+        country: "USA"
+    })
         .then(() => {
             console.log("Document successfully written!");
         })
